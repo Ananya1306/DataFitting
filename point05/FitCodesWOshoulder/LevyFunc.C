e405cf31da11bc9e073daf573569c153f890aabd
Ananya Paul <apaul029@ucr.edu>
2025-01-17 15:39:32 -0800

    Trying to fit background without the shoulder

---
+++ point05/FitCodesWOshoulder/LevyFunc.C
@@ -0,0 +1,42 @@
+#include "TH1.h"
+#include "TF1.h"
+#include "TList.h"
+
+
+Double_t levy(Double_t *x, Double_t *par){
+	
+	 if (x[0] <= par[2]){
+                   return 0.0;
+           }
+
+   return (par[0])*(sqrt(par[1]/2*TMath::Pi())*exp(-par[1]/(2*(x[0]-par[2])))*(1/(pow((x[0]-par[2]),1.5)))); //Levy Distribution
+
+//par[0] is Ampltitude, par[1] is Scale, par[2] is location
+}
+
+void LevyFunc(){
+
+   TFile *f = new TFile("../BothTreeOutAll_XfTestpoint05.root");
+   TH1F *h = (TH1F*)f->Get("pi0MxFPhi_2");
+   TH1F *hBack = (TH1F*)h->Clone("hBack");
+   TH1F *hBackFit = (TH1F*)h->Clone("hBackFit");
+   for(int i=12; i<30; i++){hBack->SetBinContent(i,0); hBack->SetBinError(i,100);}
+   hBack->Draw(); 
+  
+   TF1 *fl = new TF1("fl",levy,0,1,3); 
+   //fl->SetParameters(1500,0.02,0.0); //pi0MxFPhi_0 fit initial parameters
+   //fl->SetParameters(150,0.9,0.01); //pi0MxFPhi_1 fit initial parameters
+   fl->SetParameters(150,0.09,0.005); //pi0MxFPhi_2 fit initial parameters
+   fl->Draw("same");
+   hBack->Fit(fl,"R");
+
+   double amplitude = fl->GetParameter(0);
+   double scale = fl->GetParameter(1);
+   double location = fl->GetParameter(2);
+
+   TF1 *flfit = new TF1("fwfit",levy,0,1,4); flfit->SetParameters(amplitude,scale,location);
+  TCanvas *c2 = new TCanvas(); hBackFit->Draw("lpe"); flfit->Draw("same");
+
+
+
+} 
---
+++ point05/FitCodesWOshoulder/LevyFuncData.C
@@ -0,0 +1,40 @@
+#include "TH1.h"
+#include "TF1.h"
+#include "TList.h"
+
+
+Double_t levy(Double_t *x, Double_t *par){
+	
+	 if (x[0] <= par[2]){
+                   return 0.0;
+           }
+
+   return (par[0])*(sqrt(par[1]/2*TMath::Pi())*exp(-par[1]/(2*(x[0]-par[2])))*(1/(pow((x[0]-par[2]),1.5)))); //Levy Distribution
+
+//par[0] is Ampltitude, par[1] is Scale, par[2] is location
+}
+
+void LevyFuncData(){
+
+   TFile *f = new TFile("../EEmcNanoTreeQA_schedRun15_BlueBeam_160All_Et04pt2_xFTest_1.root");
+   TH1F *h = (TH1F*)f->Get("pi0M_BDown_xF2_phi10");
+   TH1F *hBack = (TH1F*)h->Clone("hBack");
+   TH1F *hBackFit = (TH1F*)h->Clone("hBackFit");
+   for(int i=12; i<30; i++){hBack->SetBinContent(i,0); hBack->SetBinError(i,100);} //for xF0 and xF1 i starts from 8
+   hBack->Draw(); 
+  
+   TF1 *fl = new TF1("fl",levy,0,1,3); 
+   //fl->SetParameters(1600,0.05,0.001); //pi0M_BDown_xF0_phi10 fit initial parameters 
+   //fl->SetParameters(900,0.09,0.02); ////pi0M_BDown_xF1_phi10 fit initial parameters 
+   fl->SetParameters(50,0.1,0.06); ////pi0M_BDown_xF2_phi10 fit initial parameters
+   fl->Draw("same");
+   hBack->Fit(fl,"R");
+
+   double amplitude = fl->GetParameter(0);
+   double scale = fl->GetParameter(1);
+   double location = fl->GetParameter(2);
+
+   TF1 *flfit = new TF1("fwfit",levy,0,1,4); flfit->SetParameters(amplitude,scale,location);
+  TCanvas *c2 = new TCanvas(); hBackFit->Draw("lpe"); flfit->Draw("same");
+
+} 
